faa73b9cfcbfed901a598840bfd794aa1853637a
GalahadDev <121528841+GalahadDev@users.noreply.github.com>
2025-02-24 16:05:27 -0300

    Update page.tsx

--- src/app/pedidosGenerales/page.tsx
+++ src/app/pedidosGenerales/page.tsx
@@ -375,6 +375,10 @@ const handleCalculateTotal = () => {
                           <CreditCard className="w-5 h-5 mr-3 text-gray-500" />
                           <p className="text-gray-600">Estado de pago: {pedido.Pagado}</p>
                         </div>
+                         <div className="flex items-center">
+                          <MapPin className="w-5 h-5 mr-3 text-gray-500" />
+                          <p className="text-gray-600">Vendedor: {pedido.Nombre_Vendedor}</p>
+                        </div>
 
                         {/* Fecha */}
                         <div className="flex items-center">

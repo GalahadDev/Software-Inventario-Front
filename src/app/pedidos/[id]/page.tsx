33357aec73fcc293c36d7d3f7deb3a21f6ca36a1
Nestor Llach <121528894+llach122@users.noreply.github.com>
2025-02-26 16:05:56 -0300

    Update page.tsx

--- src/app/pedidos/[id]/page.tsx
+++ src/app/pedidos/[id]/page.tsx
@@ -363,7 +363,7 @@ const PedidosPage = () => {
 
                         <div className="flex items-center">
                           <CreditCard className="w-5 h-5 mr-3 text-gray-500" />
-                          <p className="text-gray-600">Numero de Telefono: {pedido.nro_tlf}</p>
+                          <p className="text-gray-600">Numero de Telefono: {pedido.Nro_Tlf}</p>
                         </div>
       
                         {pedido.Observaciones && (

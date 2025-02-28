16bc1d55cca6e544b24ad0d90c70ae707186ce42
GalahadDev <121528841+GalahadDev@users.noreply.github.com>
2025-02-27 22:49:29 -0300

    Delete app directory

--- app/utils/dateUtils.ts
+++
@@ -1,7 +0,0 @@
-// src/app/utils/dateUtils.ts
-export const toChileDate = (date: Date): Date => {
-  // Convierte la fecha a la zona horaria de Chile y extrae solo la parte de la fecha
-  const options: Intl.DateTimeFormatOptions = { timeZone: 'America/Santiago', year: 'numeric', month: '2-digit', day: '2-digit' };
-  const dateString = date.toLocaleDateString('en-US', options);
-  return new Date(dateString);
-};
